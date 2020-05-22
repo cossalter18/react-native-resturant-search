4daa1e873e4efe661fbb63b8a3d74e29def214f6
Mike Cossalter <mikecossalter@yahoo.com>
2020-05-22 00:43:13 -0500

    search bar complete, working route to yelp

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../Components/SearchBar'
 
 
 const SearchScreen = () => {
 
+    const [term, setTerm] = useState('');
+    // going to pass down search term and the call back to change it to the search bar component
+
     return (
         <View>
-            <SearchBar />
+            <SearchBar term={term} 
+            onTermChange={newTerm => setTerm(newTerm)} 
+            onTermSubmit={()=> console.log('term was submitted')}
+            />
             <Text>Search Screen</Text>
+            <Text>{term}</Text>
         </View>
     )
 };
