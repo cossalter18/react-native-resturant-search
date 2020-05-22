70a62949b122cfdef77524f3b4a1fdf218e2cef8
Mike Cossalter <mikecossalter@yahoo.com>
2020-05-22 00:14:19 -0500

    building search bar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import SearchBar from '../Components/SearchBar'
 
 
 const SearchScreen = () => {
 
     return (
         <View>
+            <SearchBar />
             <Text>Search Screen</Text>
         </View>
     )
