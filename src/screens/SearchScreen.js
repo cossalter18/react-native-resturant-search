3234a637dc1b5a18262085e92ac6b16803be9e12
Mike Cossalter <mikecossalter@yahoo.com>
2020-05-22 18:58:42 -0500

    working navigation prop

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,36 +7,37 @@ import ResultsList from '../Components/ResultsList'
 
 
 
-const SearchScreen = () => {
+const SearchScreen = ({ navigation }) => {
+    console.log(props)
 
     const [term, setTerm] = useState('');
     // going to pass down search term and the call back to change it to the search bar component
     //any time we want to update items on the screen we use state
     const [searchApi, results, errorMessage] = useResults();
 
-   //helper function
-   const filterResultsByPrice = (price) => {
-       //price === '$' || '$$' || '$$$'
-       // for every result inside our result array we will ask if result.price is equal to the price we passed in. 
-       //this will give us customized results for each resultlist component
-       return results.filter(result => {
-           return result.price === price;
-       })
-   }
+    //helper function
+    const filterResultsByPrice = (price) => {
+        //price === '$' || '$$' || '$$$'
+        // for every result inside our result array we will ask if result.price is equal to the price we passed in. 
+        //this will give us customized results for each resultlist component
+        return results.filter(result => {
+            return result.price === price;
+        })
+    }
 
     return (
         <>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
-                onTermSubmit={ () => searchApi(term)}
+                onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             {/* <Text>We have found {results.length} results</Text> */}
             <ScrollView>
-            <ResultsList results={filterResultsByPrice('$')} title="Cost Effective"/>
-            <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier"/>
-            <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
+                <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" navigation={navigation} />
+                <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" navigation={navigation} />
+                <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" navigation={navigation} />
             </ScrollView>
         </>
     )
