392d8f963c633c2d8a292209842ade5e05ca90ac
Mike Cossalter <mikecossalter@yahoo.com>
2020-05-23 13:08:49 -0500

    rendering photos on ResultShowScreen

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,22 +1,48 @@
-import React from 'react'
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react'
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import yelp from '../API/yelp'
 
 
 
-const ResultsShowScreen = ({navigation}) => {
+const ResultsShowScreen = ({ navigation }) => {
+    const [result, setResult] = useState(null);
     const id = navigation.getParam('id')
-    console.log(id);
-    
+
+    console.log(result)
+
+    //helper function
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+    };
+
+    useEffect(() => {
+        getResult(id)
+    }, []);
+
+    if (!result){
+        return null;
+    }
 
     return (
-    <View>
-        <Text>Results Show</Text>
-    </View>
+        <View>
+            <Text>{result.name}</Text>
+            <FlatList 
+            data={result.photos}
+            keyExtractor={(photo) => photo}
+            renderItem={({ item }) => {
+                return <Image style={styles.image} source={{ uri: item }}/>
+            }}
+            />
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    image: {
+        height: 200,
+        width: 300
+    }
 });
 
 export default ResultsShowScreen;
